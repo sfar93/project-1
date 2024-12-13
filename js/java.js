778a1107208edda4ef191b225f8dea42ce3edca7
saleh farah <salehf1993@gmail.com>
2024-12-12 20:50:51 -0700

    updated html and js

--- js/java.js
+++ js/java.js
@@ -52,18 +52,25 @@ const RAPID_API_KEY = '1456abf87cmshfa52d039b76b2a2p1c6c6bjsneec8df59ca1b'
 document.addEventListener("DOMContentLoaded", async function () {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const selectAllButton = document.getElementById('select-all');
+  const api = document.querySelector('.api');
+  
 
   let selectedFoodTypes = [];
+  let selectedMealTypes = [];
+  let selectedTimeOfDay = [];
 
   // const restaurants = await getData();
 
   const applyFoodTypeFilter = () => {
     console.log(restaurants)
     const filteredRestaurants = restaurants.filter(restaurant => {
-      const hasType = restaurant.cuisines.some(cuisine => selectedFoodTypes.includes(cuisine));
+      let hasType = restaurant.cuisines.some(cuisine => selectedFoodTypes.includes(cuisine));
+       hasType = restaurant.meal_types.some(meal_type => selectedMealTypes.includes(meal_type));
       return hasType;
     })
-    console.log(filteredRestaurants)
+    console.log(filteredRestaurants);
+    
+    renderRestaurants(filteredRestaurants);
   }
 
 
@@ -90,17 +97,44 @@ document.addEventListener("DOMContentLoaded", async function () {
             return item !== event.target.value
           })
           applyFoodTypeFilter()
+        }else if (event.target.name == "mealtype"){
+          selectedMealTypes = selectedMealTypes.filter(function (item){
+
+          })
+        
         }
       } else {
         if (event.target.name == "food") {
           selectedFoodTypes.push(event.target.value);
           applyFoodTypeFilter()
-        }
-      }
 
+        }else if (event.target.name == "mealtype") {
+          selectedMealTypes.push(event.target.value);
+          applyFoodTypeFilter()
+    }
+  }
+      
 
     });
-  });
+    
+   
 
+  });
+  function renderRestaurants (restaurants) {
+       
+    api.innerHTML = '';
+    for (let i = 0; i < restaurants.length; i++){
+      const restaurant = restaurants[i];
+  
+      const art = document.createElement('article');
+      art.textContent = restaurant.name;
+      
+      api.appendChild(art);
+      
+    }
+  }
 
 });
+
+
+
